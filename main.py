ba87a66f6ea56403fd66a7ed375eaa5eda559fa7
malysh1976 <semenm1305@gmail.com>
2024-01-28 11:23:39 +0300

    доделать кнопку

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import telebot
 import requests
  
  
-token='' 
+token='6365100027:AAESUZPeAmVhDtcAPwSGJPl7IS8LINciRIs' 
  
 bot=telebot.TeleBot(token) 
  
@@ -11,30 +11,41 @@ def reqapi(ip):
     r=requests.get(url).json() 
     return(r) 
  
+def generator_keyboards(ListNameBTN, NumbersColumns=2):
+    keyboards=telebot.types.ReplyKeyboardMarkup(row_width=NumbersColumns, resize_keyboard=True)
+    btn_names=[telebot.types.KeyboardButton(text=x) for x in ListNameBTN]
+    keyboards.add(*btn_names)
+    return keyboards       
+
+
+
 @bot.message_handler(commands=['start']) 
-def start(message): 
-    msg=bot.send_message(message.chat.id,f'Привет, {message.from_user.username}\n  Введи ip:  ') 
-    bot.register_next_step_handler(msg, getip) 
+def start(message):
+    bot.send_message(message.chat.id,f'Привет', reply_markup=generator_keyboards(['Информация об ip']))
  
- 
- 
-def getip(message): 
-    ip = message.text 
-    res=reqapi(ip)
-    msg=f' По вашему ip - {res["ip"]}\
-    \n Город - {res["city"]}\
-    \n Регион - {res["region"]}\
-    \n Страна - {res["country"]}\
-    \n Координаты - {res["loc"]}\
-    \n Часовой пояс - {res["timezone"]}'
-    
-    bot.send_message(message.chat.id,msg) 
+@bot.message_handler(func=lambda x :x.text)
+def text(message):
+    text=message.text
+    if text=='Информация об ip':
+        def getip(message): 
+            ip = message.text 
+            res=reqapi(ip)
+            print(res)
+            msg=f' По вашему ip - {res["ip"]}\
+            \n Город - {res["city"]}\
+            \n Регион - {res["region"]}\
+            \n Страна - {res["country"]}\
+            \n Координаты - {res["loc"]}\
+            \n Часовой пояс - {res["timezone"]}'
+            bot.send_message(message.chat.id,msg) 
+
+
+        
+
+
 
 if __name__=='__main__': 
     bot.infinity_polling()
 
 
-
-
-
-
+ 
